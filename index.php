94b816df7e624258fefeeb4cae080b771d4d3b19
Geekcoder127 <awesome2723@oultook.com>
2017-04-23 21:33:47 +0100

    Added Current Advancements

---
+++ index.php
@@ -0,0 +1,19 @@
+<html><head>
+	<!-- VENDOR LINKS -->
+	<link href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,600,700" rel="stylesheet">
+
+	<!-- RESOURCE LINKS -->	
+	<meta charset="utf-8">
+	<link rel="stylesheet" type="text/css" href="resources/css/main.css">
+	<title>OmniNews | Home</title>
+	<link rel="icon" type="image/ico" href="favicon.ico">
+</head>
+
+<body>
+
+	<?php
+		include "resources/htmlextras/nav.html";
+	?>
+
+</body>
+</html>
