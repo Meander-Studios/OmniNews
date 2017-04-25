dbc9608f18a9f86aefa9a1cab23c235a8b52bebe
Geekcoder127 <awesome2723@oultook.com>
2017-04-25 22:00:53 +0100

    Front Page Edits

--- index.php
+++ index.php
@@ -1,6 +1,8 @@
 <html><head>
 	<!-- VENDOR LINKS -->
 	<link href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,600,700" rel="stylesheet">
+	<script src="http://code.jquery.com/jquery-1.11.3.min.js"></script> 
+	<script src="vendors/jquery/mini-jquery-bgswitcher.js"></script> 
 
 	<!-- RESOURCE LINKS -->	
 	<meta charset="utf-8">
@@ -21,4 +23,24 @@
 
 	</form>
 </body>
+<script>
+	var params = {
+		images: [
+			"resources/media/img/backgrounds/1.jpeg",
+			"resources/media/img/backgrounds/2.jpeg",
+			"resources/media/img/backgrounds/3.jpeg",
+			"resources/media/img/backgrounds/4.jpeg",
+			"resources/media/img/backgrounds/5.jpeg",
+			"resources/media/img/backgrounds/6.jpeg",
+
+		],
+		el: 'body',
+		animDuration: 500,
+		slideDuration: 4000,
+		/*gradient: "linear-gradient(rgba(0,0,0,0.3),rgba(0,0,0,0.3))",*/
+		vignette: false,
+	};
+
+	bgSlider(params);
+</script>
 </html>
